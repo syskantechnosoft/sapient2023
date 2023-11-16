2bf4318793063fde89610dd97502ab218411ba9c
Siva OS <syskantechnosoft@users.noreply.github.com>
2023-11-16 08:21:39 +0530

    Add files via upload

---
+++ Day22 Kafka/springkafka/src/main/java/com/sapient/springkafka/controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.sapient.springkafka.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sapient.springkafka.service.ProducerService;
+
+@RestController
+@RequestMapping("/kafka")
+public final class KafkaController {
+    private final ProducerService producerService;
+
+    public KafkaController(ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam String message) {
+        producerService.sendMessage(message);
+    }
+}
